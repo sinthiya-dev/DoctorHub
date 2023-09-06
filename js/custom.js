082dcf2728e37b0eeade36a903de7a70a8944407
Umme Habiba Sinthiya <ummehabibas761@gmail.com>
2023-09-07 01:01:42 +0600

    history

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,13 @@
 (function ($) {
+  //  myFunction() {
+  //   var txt;
+  //   if (confirm("Press a button!")) {
+  //     txt = "You pressed OK!";
+  //   } else {
+  //     txt = "You pressed Cancel!";
+  //   }
+  //   document.getElementById("demo").innerHTML = txt;
+  // };
     "use strict";
 
     // Spinner
